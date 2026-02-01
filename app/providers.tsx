08ac446f0f79162bf3241261582f21abe7583a38
kkang123 <one98kr@gmail.com>
2026-02-01 19:57:43 +0900

    ✨ init : 초기 세팅

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+// tanStack Query Provider 설정
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  const [client] = useState(() => new QueryClient());
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
