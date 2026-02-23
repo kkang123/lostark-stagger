57040ad776f4e5a5249edd5637db70b5f4c547db
kkang123 <one98kr@gmail.com>
2026-02-23 22:43:19 +0900

    🚚 _components를 dev로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import { Providers } from "./providers";
-import AgentationClient from "@/_components/AgentationClient";
+import AgentationClient from "@/dev/AgentationClient";
 
 import "./globals.css";
 
